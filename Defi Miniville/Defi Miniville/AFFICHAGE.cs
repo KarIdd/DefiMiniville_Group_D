53d2bf9a8ff9c68766814a00d4abef169619f877
ClementMiermont <clementmi_artworkandgraph@outlook.com>
2021-10-25 14:39:49 +0200

    25/10/21 14:40

---
+++ Defi Miniville/Defi Miniville/AFFICHAGE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Defi_Miniville
+{
+    class AFFICHAGE
+    {
+        public void Affichage()
+        {
+            Console.WriteLine("+ ----------------------------------- +");
+            Console.WriteLine("|  BIENVENUE DANS CE JEU DE MINIVILE  |");
+            Console.WriteLine("+ ----------------------------------- +");
+        }
+
+
+    }
+}
