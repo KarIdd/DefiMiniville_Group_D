0c539eecb5652090a12089ed68ba801037d253d4
ClementMiermont <clementmi_artworkandgraph@outlook.com>
2021-10-25 11:32:52 +0200

    25/10/21 11:32

---
+++ Defi Miniville/Die.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Die
+{
+	public Die ()
+	{
+		public int NbFaces;
+		private Random = new Random();
+		public int Face { get, private set};
+
+
+	}
+}
