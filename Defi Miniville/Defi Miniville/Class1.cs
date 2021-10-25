8f0db19ffc9252999e80e036c42d0d605e2c812e
Edragaled <91267250+Edragaled@users.noreply.github.com>
2021-10-25 11:22:36 +0200

    25/10/21 11:22

---
+++ Defi Miniville/Defi Miniville/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Defi_Miniville
+{
+    class Pile
+    {
+        List<Card> cards = List<Card>();
+        
+        public Card DrawCard()
+        {
+            Card drawnCard = cards[0];
+            return
+        }
+    }
+}
