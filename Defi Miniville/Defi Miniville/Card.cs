ede4207c0f27e6eeb2dd5de056632febb1a31ef4
FLATRES Johan <76738358+KarIdd@users.noreply.github.com>
2021-10-25 15:24:19 +0200

    25/10/21 15'24"

--- Defi Miniville/Defi Miniville/Card.cs
+++ Defi Miniville/Defi Miniville/Card.cs
@@ -5,7 +5,7 @@ namespace Defi_Miniville
 {
 	public class Card
 	{
-		List<CardsInfo> deck = new List<CardsInfo>();
+		public static List<CardsInfo> deck = new List<CardsInfo>();
 
 		public Card()
         {
@@ -17,18 +17,22 @@ namespace Defi_Miniville
 			deck.Add(new CardsInfo(Id, Color, Cost, Name, Number, Effect, MinDice, MaxDice, Gain));
 		}
 
-		public static CardsInfo GetCard(int Id, List<CardsInfo> deck)
+		public CardsInfo GetCard(int Id)
 		{
-			foreach (CardsInfo card in deck)
-			{
-				if (card.Id == Id)
-				{
-					return card;
-				}
-			}
-			return new CardsInfo();
+			return (deck[Id]);
+		}
+
+		public void CreateDeck()
+        {
+			Card card = new Card();
+			card.AddCard(0, "Blue", 1, "Wheat field", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(1, "Blue", 2, "Farm", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(2, "Green", 1, "Baker's shop", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(3, "Red", 2, "Café", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(4, "Green", 2, "Wheat field", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(5, "Blue", 2, "Wheat field", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(6, "Red", 4, "Wheat field", 6, "Get 1 coin", 1, 1, 1);
+			card.AddCard(7, "Blue", 6, "Wheat field", 6, "Get 1 coin", 1, 1, 1);
 		}
 	}
-
-    
 }
