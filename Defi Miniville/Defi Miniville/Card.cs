3ed94a761e5ac2480e7bbcd6802b04f3fdd047b6
FLATRES Johan <76738358+KarIdd@users.noreply.github.com>
2021-10-25 16:26:27 +0200

    25/10/21 16'26"

--- Defi Miniville/Defi Miniville/Card.cs
+++ Defi Miniville/Defi Miniville/Card.cs
@@ -24,12 +24,20 @@ namespace Defi_Miniville
 			card.AddCard(3, "Red", 2, "Café", 6, "Get 1 coin from the player that rolled the die", 3, 3, 1);
 			card.AddCard(4, "Green", 2, "Grocery", 6, "Get 3 coins", 4, 4, 3);
 			card.AddCard(5, "Blue", 3, "Forest", 6, "Get 1 coin", 5, 5, 1);
-			card.AddCard(6, "Red", 4, "Restaurant", 6, "Get 2 coins from the player that rolled the die", 5, 5, 2);
+			card.AddCard(6, "Red", 4, "Restaurant", 6, "Get 2 coins from the player that rolled the die", 9, 10, 2);
 			card.AddCard(7, "Blue", 6, "Stadium", 4, "Get 4 coins", 6, 6, 4);
 			card.AddCard(8, "Green", 8, "Business center", 4, "Get 6 coins", 6, 6, 6);
 			card.AddCard(9, "Green", 7, "TV station", 4, "Get 5 coins from the player that rolled the die", 6, 6, 5);
-			card.AddCard(10, "Green", 5, "")
-
+			card.AddCard(10, "Green", 5, "Dairy", 6, "Get 3 coins foreach shop that you have", 7, 7, 0);
+			card.AddCard(11, "Green", 3, "Furniture factory", 6, "Get 3 coins foreach forest or mine that you have", 8, 8, 0);
+			card.AddCard(12, "Blue", 6, "Mine", 6, "Get 5 coins", 9, 9, 5);
+			card.AddCard(13, "Blue", 3, "Orchard", 6, "Get 3 coins", 10, 10, 3);
+			card.AddCard(14, "Green", 2, "Greengrocer's", 6, "Get 2 coins foreach wheat field and orchard that you have", 11, 12, 0);
 		}
+
+		public void ActiveCards()
+        {
+
+        }
 	}
 }
