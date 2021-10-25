f17d3709372d7184f442257f1f08b8f78959513d
Edragaled <91267250+Edragaled@users.noreply.github.com>
2021-10-25 15:40:52 +0200

    25/10 15h25

--- Defi Miniville/Defi Miniville/Card.cs
+++ Defi Miniville/Defi Miniville/Card.cs
@@ -7,12 +7,18 @@ namespace Defi_Miniville
 	{
 		List<CardsInfo> deck = new List<CardsInfo>();
 
+		public static Dictionary<int, CardsInfo> cardData = new Dictionary<int, CardsInfo>()
+		{
+			{ 0, new CardsInfo(0, "Bleue", 2, "Champs de blé", 7, "Gagne des pièces", 5, 7, 2) },
+			{ 1, new CardsInfo(1, "Vert", 2, "zoicnoiz") },
+		};
+
 		public void AddCard(int Id, string Color, int Cost, string Name, int Number, string Effect, int MinDice, int MaxDice, int Gain)
 		{
 			deck.Add(new CardsInfo(Id, Color, Cost, Name, Number, Effect, MinDice, MaxDice, Gain));
 		}
 
-		public static CardsInfo GetCard(int Id)
+		public CardsInfo GetCard(int Id)
 		{
 			switch (Id)
             {
