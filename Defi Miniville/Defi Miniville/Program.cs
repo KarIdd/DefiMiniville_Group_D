0e1f1aafc551f64f1bd116c8418fb5c4d00b7080
Nakso13 <nakso133@gmail.com>
2021-10-25 11:02:39 +0200

    25/10/21 11"02

---
+++ Defi Miniville/Defi Miniville/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Defi_Miniville
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
