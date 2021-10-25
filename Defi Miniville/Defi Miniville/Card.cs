7dbe45cb51af25a7164618064ee10aa58eeec940
FLATRES Johan <76738358+KarIdd@users.noreply.github.com>
2021-10-25 14:23:46 +0200

    25/10/21 14'23"

--- Defi Miniville/Defi Miniville/Card.cs
+++ Defi Miniville/Defi Miniville/Card.cs
@@ -9,7 +9,20 @@ namespace Defi_Miniville
 
 		public void AddCard(int Id, string Color, int Cost, string Name, int Number, string Effect, int MinDice, int MaxDice, int Gain)
         {
-			deck.Add(CardsInfo(Id, Color, Cost, Name, Number, Effect, MinDice, MaxDice, Gain));
+			deck.Add(new CardsInfo(Id, Color, Cost, Name, Number, Effect, MinDice, MaxDice, Gain));
         }
+
+		public CardsInfo GetCard(int Id)
+        {
+			foreach (CardsInfo card in deck)
+            {
+				if (card.Id == Id)
+                {
+					return card;
+                }
+            }
+			return new CardsInfo();
+
+		}
 	}
 }
