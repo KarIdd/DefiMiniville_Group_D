a9f4cf9a9dfed49c0f0b0d40e689dd6b885327d4
Edragaled <91267250+Edragaled@users.noreply.github.com>
2021-10-25 11:40:18 +0200

    25/10/21  11h40

--- Defi Miniville/Defi Miniville/Pile.cs
+++ Defi Miniville/Defi Miniville/Pile.cs
@@ -8,6 +8,16 @@ namespace Defi_Miniville
     {
         List<Card> cards = List<Card>();
 
+        public List<Card> GetCards(Pile pile, string cardColor)
+        {
+            List<Card> cards = List<Card>();
+            foreach (Card card in pile)
+            {
+                if (card)
+            }
+            return cards
+        }
+
         public Card DrawCard()
         {
             Card drawnCard = cards[-1];
@@ -16,3 +26,4 @@ namespace Defi_Miniville
 
     }
 }
+
