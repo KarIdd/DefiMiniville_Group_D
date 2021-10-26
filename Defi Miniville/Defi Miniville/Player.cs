1ece35b590ac43d7bb270423440506e57b20f194
Nakso13 <nakso133@gmail.com>
2021-10-26 11:23:53 +0200

    26/10/21 11"23

--- Defi Miniville/Defi Miniville/Player.cs
+++ Defi Miniville/Defi Miniville/Player.cs
@@ -16,7 +16,12 @@ namespace Defi_Miniville
         {
             if(Pieces > Card.GetCard(Id).Cost && Card.GetCard(Id).Number > 0)
             {
-                
+                // Decreasing the card number
+                int cardNumber = Card.CardShop[Id].Number;
+                Card.CardShop[Id] = new CardsInfo(Id, Card.CardShop[Id].Color, Card.CardShop[Id].Cost, Card.CardShop[Id].Name, cardNumber-1, Card.CardShop[Id].Effect, Card.CardShop[Id].MinDice, Card.CardShop[Id].MaxDice, Card.CardShop[Id].Gain);
+
+                // Adding the card to player's pile.
+                //EMPILER
             }
         }
     }
