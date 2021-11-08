4072666c423db9facbea8325e2fbec2484bf451c
FLATRES Johan <76738358+KarIdd@users.noreply.github.com>
2021-11-08 09:53:51 +0100

    08/11/21 9'53"

--- Defi Miniville (Interface Graphique)/Minivilles Graphique/obj/Debug/netcoreapp3.1/App.g.cs
+++ Defi Miniville (Interface Graphique)/Minivilles Graphique/obj/Debug/netcoreapp3.1/App.g.cs
@@ -45,7 +45,7 @@ namespace Minivilles_Graphique {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Minivilles_Graphique {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             Minivilles_Graphique.App app = new Minivilles_Graphique.App();
             app.InitializeComponent();
